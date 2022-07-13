c8c57b57531529eae48bdf919839c4caae867449
7ih <ucfspartan@gmail.com>
2022-07-13 15:34:14 -0400

    plugin is working

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,11 +1,19 @@
-(function(s,t,d){"use strict";function u(e){if(e&&e.__esModule)return e;var o=Object.create(null);return e&&Object.keys(e).forEach(function(r){if(r!=="default"){var n=Object.getOwnPropertyDescriptor(e,r);Object.defineProperty(o,r,n.get?n:{enumerable:!0,get:function(){return e[r]}})}}),o.default=e,Object.freeze(o)}var a=u(s);d.useNest(t.persist),cumcord.pluginData.showSettings(),t.persist.store.locked=!1,cumcord.patcher.injectCSS(`
+(function(h,t,w,c){"use strict";function v(e){if(e&&e.__esModule)return e;var l=Object.create(null);return e&&Object.keys(e).forEach(function(s){if(s!=="default"){var g=Object.getOwnPropertyDescriptor(e,s);Object.defineProperty(l,s,g.get?g:{enumerable:!0,get:function(){return e[s]}})}}),l.default=e,Object.freeze(l)}var o=v(h);const b=c.webpack.findByDisplayName("FormItem"),k=c.webpack.findByDisplayName("FormText"),n={...c.webpack.findByProps("marginBottom20"),...c.webpack.findByProps("formText"),...c.webpack.findByDisplayName("Flex")};var E=e=>o.createElement(b,{title:e.title,required:e.required,className:`${n.Direction.VERTICAL} ${n.Justify.START} ${n.Align.STRETCH} ${n.Wrap.NO_WRAP} ${n.marginBottom20}`},e.children,e.note&&o.createElement(k,{className:`${n.description} ${n.marginTop8}`},e.note));const D=c.webpack.findByDisplayName("TextInput");var d=e=>{const l=e.children;return delete e.children,o.createElement(E,{title:l,required:e.required,note:e.note},o.createElement(D,{...e}))};console.log("run code"),typeof t.store.passcode!="string"&&(t.store.passcode=""),typeof t.store.locked!="boolean"&&(t.store.locked=!1);let P=function(e){e===t.store.passcode&&$()};const a=document.createElement("div");a.id="pclwrapper";const T=o.createElement("div",{id:"pclinputwrapper"},o.createElement(d,{id:"pclinput",type:"password",defaultValue:"",onKeyDown:p,onChange:P,onPaste:i},"Passcode"));w.ReactDOM.render(T,a),document.body.appendChild(a),cumcord.patcher.injectCSS(`
 #pclwrapper {  
     position: absolute;
-    margin: auto;
     top: 0;
-    right: 0;
-    bottom: 0;
     left: 0;
     width: 100%;
     height: 100%;
-}`);let i=function(){t.persist.store.locked=!0,cumcord.patcher.injectCSS(".app-2CXKsg {filter: blur(.8rem);}");const e=document.createElement("div");e.id="pclwrapper",document.getElementsByClassName("app-3xd6d0")[0].appendChild(e)},c=async e=>{e.code=="F12"&&!t.persist.store.locked&&(e.preventDefault(),i())};var l={onLoad(){document.addEventListener("keydown",c)},onUnload(){document.removeEventListener("keydown",c)},settings:()=>a.createElement(TextInput,{note:"You will have to enter this sequence to unlock discord",defaultValue:"",onChange:e=>t.persist.store.passcode=e},"Passcode")};return l})(cumcord.modules.common.React,cumcord.pluginData,cumcord.utils);
+    display: none;
+}
+#pclinputwrapper {
+    position: absolute;
+    margin: auto;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 300px;
+    height: 50px;
+}`);let u=function(e){cumcord.patcher.injectCSS(`.notDevTools-1zkgfK {filter: blur(${e?.8:0}rem);}`)},m=function(){t.store.passcode.length!==0&&(u(!0),r("pclinput").maxLength=t.store.passcode.length,a.style.display="block",r("pclinput").focus(),t.store.locked=!0)},$=function(){u(!1),a.style.display="none",pclinput.value="",t.store.locked=!1},f=function(e){e.code=="F12"&&!t.store.locked&&(e.preventDefault(),m())};function r(e){return document.getElementById(e)}function i(e){e.preventDefault()}function p(e){e.key===" "&&e.preventDefault()}function y(e){r("pclpass1").value.length===0&&r("pclpass2").value.length==0?t.store.passcode="":r("pclpass1").value===r("pclpass2").value?t.store.passcode=e:t.store.passcode=""}m();var B={onLoad(){document.addEventListener("keydown",f)},onUnload(){document.removeEventListener("keydown",f),a.remove()},settings:()=>o.createElement("div",null,o.createElement(d,{id:"pclpass1",type:"password",note:"You will have to enter this password to unlock discord (no spaces)",defaultValue:t.store.passcode,onKeyDown:p,onChange:y,onPaste:i},"Passcode"),o.createElement(d,{id:"pclpass2",type:"password",note:"Passwords must match in order for you to be able to lock discord",defaultValue:t.store.passcode,onKeyDown:p,onChange:y,onPaste:i},"Confirm Passcode"))};return B})(cumcord.modules.common.React,cumcord.pluginData.persist,cumcord.modules.common,cumcord.modules);
--- src/index.jsx
+++ src/index.jsx
@@ -1,54 +1,155 @@
-import { persist } from '@cumcord/pluginData';
-import { useNest } from '@cumcord/utils';
+console.log("run code")
 
-useNest(persist);
-cumcord.pluginData.showSettings();
+import { store as st } from '@cumcord/pluginData/persist';
+import { ReactDOM } from '@cumcord/modules/common';
+import TextInput from './Components/TextInput';         // components taken from ugly-patootie's plugins
 
-persist.store.locked = false
+//st.locked = false
+
+// check if variables are set
+if (typeof st.passcode !== "string")    st.passcode = "";
+if (typeof st.locked !== "boolean")     st.locked = false;
+
+//
+// create password input
+//
+
+let passwordHandler = function(val) {
+    if (val === st.passcode) {
+        unlock();
+    }
+}
+
+// #pclwrapper is neccessary for blocking inputs while locked
+// also it has to be added with appenChild as to not replace every single other element
+
+const pclwrapper = document.createElement("div");
+pclwrapper.id = "pclwrapper";
+
+// #pclinputwrapper is so the textinput is displayed properly
+
+const passcodeEntry = (
+    <div id="pclinputwrapper">
+        <TextInput
+            id="pclinput"
+            type="password"
+            defaultValue=""
+            onKeyDown={supressSpaces}
+            onChange={passwordHandler}
+            onPaste={preventAction}
+        >
+            Passcode
+        </TextInput>
+    </div>
+)
+
+ReactDOM.render(passcodeEntry, pclwrapper)
+document.body.appendChild(pclwrapper);      // if wrapper isnt under body it will sometimes break input focusing
 
 cumcord.patcher.injectCSS(`
 #pclwrapper {  
     position: absolute;
-    margin: auto;
     top: 0;
-    right: 0;
-    bottom: 0;
     left: 0;
     width: 100%;
     height: 100%;
+    display: none;
+}
+#pclinputwrapper {
+    position: absolute;
+    margin: auto;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 300px;
+    height: 50px;
 }`);
 
-let lock = function() {
-    persist.store.locked = true
-    cumcord.patcher.injectCSS(`.app-2CXKsg {filter: blur(.8rem);}`);
-    const node = document.createElement("div");
-    node.id = "pclwrapper";
-    document.getElementsByClassName("app-3xd6d0")[0].appendChild(node);
+//
+// end of password input
+//
+
+let blurScreen = function(blur) {
+    cumcord.patcher.injectCSS(`.notDevTools-1zkgfK {filter: blur(${blur ? .8 : 0}rem);}`);
 }
 
-let lockKey = async r => { 
-    if (r.code == "F12" && !persist.store.locked) {
-        r.preventDefault()
-        lock()
+let lock = function() {
+    if (st.passcode.length === 0) return;
+
+    blurScreen(true);
+    $("pclinput").maxLength = st.passcode.length;
+    pclwrapper.style.display = "block";
+    $("pclinput").focus();
+    st.locked = true;
+}
+let unlock = function() {
+    blurScreen(false);
+    pclwrapper.style.display = "none";
+    pclinput.value = "";
+    st.locked = false;
+}
+
+let lockKey = function(e) { 
+    if  (e.code == "F12" && !st.locked) {
+        e.preventDefault();
+        lock();
     } 
 } 
 
+function $(id) {
+    return document.getElementById(id);
+}
+
+function preventAction(e) {
+    e.preventDefault();
+};
+function supressSpaces(e) {
+    if (e.key === " ") e.preventDefault();
+}
+
+function checkIfPasscodesMatch(val) {
+    if (($("pclpass1").value.length === 0) && ($("pclpass2").value.length == 0))    st.passcode = "";
+    else if ($("pclpass1").value === $("pclpass2").value)                           st.passcode = val;
+    else                                                                            st.passcode = "";
+}
+
+// lock discord when opened (if it was already locked)
+lock();
+
 export default {
     onLoad() { 
-        document.addEventListener("keydown", lockKey) ;
+        document.addEventListener("keydown", lockKey);
     }, 
-
     onUnload() { 
-        document.removeEventListener("keydown", lockKey) ;
+        document.removeEventListener("keydown", lockKey);
+        pclwrapper.remove();
     },
 
     settings: () => (
-        <TextInput
-            note='You will have to enter this sequence to unlock discord'
-            defaultValue=""
-            onChange={val => (persist.store.passcode = val)}
-        >
-            Passcode
-        </TextInput>
-      ),
+        <div>
+            <TextInput
+                id="pclpass1"
+                type="password"
+                note='You will have to enter this password to unlock discord (no spaces)'
+                defaultValue={st.passcode}
+                onKeyDown={supressSpaces}
+                onChange={checkIfPasscodesMatch}
+                onPaste={preventAction}
+            >
+                Passcode
+            </TextInput>
+            <TextInput
+                id="pclpass2"
+                type="password"
+                note='Passwords must match in order for you to be able to lock discord'
+                defaultValue={st.passcode}
+                onKeyDown={supressSpaces}
+                onChange={checkIfPasscodesMatch}
+                onPaste={preventAction}
+            >
+                Confirm Passcode
+            </TextInput>
+        </div>
+    )
 }
